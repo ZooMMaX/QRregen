16a25e2c6d6642a6862149aacfc652e1788af588
ZooMMaX <zoommax2009@gmail.com>
2026-08-20 11:35:27 +0300

    configure vite for nginx proxy on /qrregen

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/qrregen/",
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3000,
     strictPort: true,
+    allowedHosts: ["nh.zoommax.space", "localhost", "127.0.0.1"],
     hmr: {
       port: 3000,
+      clientPort: 443,
     },
   },
 });
